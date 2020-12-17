150acf1b9272fe2bc1f19b3e176896bdbb2d4916
mark <marx.design@icloud.com>
2020-12-18 00:51:30 +0500

    minor

--- src/app/elements/forms/renderer/renderer.component.ts
+++
@@ -1,8 +0,0 @@
-import { FormGroup } from "@angular/forms";
-import { FieldBase } from "../../../components/acts/act-form/models/fields/field-base.model";
-import { SelectField } from "../../../components/acts/act-form/models/fields/select-filed.model";
-
-export interface RendererComponent {
-  field: SelectField<any>;
-  form: FormGroup;
-}
