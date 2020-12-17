5090bd989ae0ec60587d7c2d38611cb98dcd6b20
mark <marx.design@icloud.com>
2020-12-18 00:50:40 +0500

    add Filter directive

--- src/app/directives/directives.module.ts
+++ src/app/directives/directives.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { HostDirective } from "./host.directive";
 import { LookUpDirective } from "./look-up.directive";
+import { FitlterHostDirective } from "./filter-host.directive";
 
 @NgModule({
-  declarations: [HostDirective, LookUpDirective],
+  declarations: [HostDirective, LookUpDirective, FitlterHostDirective],
   imports: [CommonModule],
-  exports: [HostDirective, LookUpDirective],
+  exports: [HostDirective, LookUpDirective, FitlterHostDirective],
 })
 export class DirectivesModule {}
